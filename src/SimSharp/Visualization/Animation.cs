b0e5a02666c4062f4c275135d65e1735f853630b
KlaraDurst <klara.durst@gmx.at>
2020-04-04 18:39:40 +0200

    integrate AnimationProps in Animation

--- src/SimSharp/Visualization/Animation.cs
+++ src/SimSharp/Visualization/Animation.cs
@@ -9,102 +9,82 @@ namespace SimSharp.Visualization {
   public class Animation {
     public enum Shape { rectangle, ellipse, polygon }
 
-    public Rectangle Rectangle0 { get; }
-    public Rectangle Rectangle1 { get; }
-
-    public Ellipse Ellipse0 { get; }
-    public Ellipse Ellipse1 { get; }
-
-    public Polygon Polygon0 { get; }
-    public Polygon Polygon1 { get; }
-
     public string Name { get; }
     public Shape Type { get; }
-    public string FillColor { get; }
-    public string LineColor { get; }
-    public int LineWidth { get; }
-    public DateTime Time0 { get; }
-    public DateTime Time1 { get; }
-    public bool Keep { get; }
 
     private Simulation env;
     private StringWriter stringWriter;
     private JsonTextWriter writer;
+    private List<AnimationProps> propsList;
     private List<AnimationUnit> units;
 
     #region Constructors
     public Animation(string name, Rectangle rectangle0, Rectangle rectangle1, DateTime time0, DateTime time1, string fillColor, string lineColor, int lineWidth, bool keep, Simulation env) 
-      : this(name, Shape.rectangle, fillColor, lineColor, lineWidth, time0, time1, keep, env) {
-      Rectangle0 = rectangle0;
-      Rectangle1 = rectangle1;
+      : this(name, Shape.rectangle, time0, time1, env) {
+      AnimationProps props = new AnimationProps(rectangle0, rectangle1, time0, time1, fillColor, lineColor, lineWidth, keep);
+      propsList.Add(props);
 
       if (env.FillAnimation)
-        Initialize();
+        Initialize(props);
     }
 
     public Animation(string name, Ellipse ellipse0, Ellipse ellipse1, DateTime time0, DateTime time1, string fillColor, string lineColor, int lineWidth, bool keep, Simulation env)
-      : this(name, Shape.ellipse, fillColor, lineColor, lineWidth, time0, time1, keep, env) {
-      Ellipse0 = ellipse0;
-      Ellipse1 = ellipse1;
+      : this(name, Shape.ellipse, time0, time1, env) {
+      AnimationProps props = new AnimationProps(ellipse0, ellipse1, time0, time1, fillColor, lineColor, lineWidth, keep);
+      propsList.Add(props);
 
       if (env.FillAnimation)
-        Initialize();
+        Initialize(props);
     }
 
     public Animation(string name, Polygon polygon0, Polygon polygon1, DateTime time0, DateTime time1, string fillColor, string lineColor, int lineWidth, bool keep, Simulation env)
-      : this(name, Shape.polygon, fillColor, lineColor, lineWidth, time0, time1, keep, env) {
-      Polygon0 = polygon0;
-      Polygon1 = polygon1;
+      : this(name, Shape.polygon, time0, time1, env) {
+      AnimationProps props = new AnimationProps(polygon0, polygon0, time0, time1, fillColor, lineColor, lineWidth, keep);
+      propsList.Add(props);
 
       if (env.FillAnimation)
-        Initialize();
+        Initialize(props);
     }
 
-    private Animation(string name, Shape type, string fillColor, string lineColor, int lineWidth, DateTime time0, DateTime time1, bool keep, Simulation env) {
+    private Animation(string name, Shape type, DateTime time0, DateTime time1, Simulation env) {
       CheckTime(time0, time1);
 
       Name = name;
       Type = type;
-      FillColor = fillColor;
-      LineColor = lineColor;
-      LineWidth = LineWidth;
-      Time0 = time0;
-      Time1 = time1;
-      Keep = keep;
       this.env = env;
     }
     #endregion
 
-    private void Initialize() {
+    private void Initialize(AnimationProps props) {
       stringWriter = new StringWriter();
       writer = new JsonTextWriter(stringWriter);
       units = new List<AnimationUnit>();
 
-      if (ShapesEqual() && Keep) {
-        AnimationUnit unit = new AnimationUnit(Time0, Time0.AddSeconds(1), 1);
-        unit.AddFrame(GetInitFrame(0));
+      if (ShapesEqual(props) && props.Keep) {
+        AnimationUnit unit = new AnimationUnit(props.Time0, props.Time0.AddSeconds(1), 1);
+        unit.AddFrame(GetInitFrame(props, 0));
         units.Add(unit);
       }
-      else if (!ShapesEqual() && Time0.Equals(Time1) && Keep) {
-        AnimationUnit unit = new AnimationUnit(Time0, Time0.AddSeconds(1), 1);
-        unit.AddFrame(GetInitFrame(1));
+      else if (!ShapesEqual(props) && props.Time0.Equals(props.Time1) && props.Keep) {
+        AnimationUnit unit = new AnimationUnit(props.Time0, props.Time0.AddSeconds(1), 1);
+        unit.AddFrame(GetInitFrame(props, 1));
         units.Add(unit);
       }
-      else if (ShapesEqual() && !Time0.Equals(Time1) && !Keep) {
-        AnimationUnit firstUnit = new AnimationUnit(Time0, Time0.AddSeconds(1), 1);
-        firstUnit.AddFrame(GetInitFrame(0));
+      else if (ShapesEqual(props) && !props.Time0.Equals(props.Time1) && !props.Keep) {
+        AnimationUnit firstUnit = new AnimationUnit(props.Time0, props.Time0.AddSeconds(1), 1);
+        firstUnit.AddFrame(GetInitFrame(props, 0));
         units.Add(firstUnit);
 
-        AnimationUnit secondUnit = new AnimationUnit(Time1, Time1.AddSeconds(1), 1); // TODO
+        AnimationUnit secondUnit = new AnimationUnit(props.Time1, props.Time1.AddSeconds(1), 1); // TODO
         secondUnit.AddFrame(GetRemoveFrame());
         units.Add(secondUnit);
       }
-      else if (!ShapesEqual() && !Time0.Equals(Time1)) {
-        int frameNumber = Convert.ToInt32((Time1 - Time0).TotalSeconds / env.AnimationBuilder.Props.TimeStep);
-        AnimationUnit animationUnit = Keep ? new AnimationUnit(Time0, Time1, frameNumber) : new AnimationUnit(Time0, Time1.AddSeconds(1), frameNumber + 1);
-        animationUnit.AddFrame(GetInitFrame(0));
+      else if (!ShapesEqual(props) && !props.Time0.Equals(props.Time1)) {
+        int frameNumber = Convert.ToInt32((props.Time1 - props.Time0).TotalSeconds / env.AnimationBuilder.Props.TimeStep);
+        AnimationUnit animationUnit = props.Keep ? new AnimationUnit(props.Time0, props.Time1, frameNumber) : new AnimationUnit(props.Time0, props.Time1.AddSeconds(1), frameNumber + 1);
+        animationUnit.AddFrame(GetInitFrame(props, 0));
 
-        foreach (List<int> i in GetInterpolation(GetTransformation(0), GetTransformation(1), frameNumber - 2)) {
+        foreach (List<int> i in GetInterpolation(GetTransformation(props, 0), GetTransformation(props, 1), frameNumber - 2)) {
           writer.WritePropertyName(Name);
           writer.WriteStartObject();
 
@@ -125,7 +105,7 @@ namespace SimSharp.Visualization {
 
         writer.WritePropertyName("t");
         writer.WriteStartArray();
-        foreach (int t in GetTransformation(1)) {
+        foreach (int t in GetTransformation(props, 1)) {
           writer.WriteValue(t);
         }
         writer.WriteEndArray();
@@ -134,7 +114,7 @@ namespace SimSharp.Visualization {
         animationUnit.AddFrame(writer.ToString());
         writer.Flush();
 
-        if (!Keep) {
+        if (!props.Keep) {
           animationUnit.AddFrame(GetRemoveFrame());
         }
 
@@ -191,7 +171,7 @@ namespace SimSharp.Visualization {
         throw new ArgumentException("time0 can not be in the past");
     }
 
-    private string GetInitFrame(int z) {
+    private string GetInitFrame(AnimationProps props, int z) {
       writer.WritePropertyName(Name);
       writer.WriteStartObject();
 
@@ -199,20 +179,20 @@ namespace SimSharp.Visualization {
       writer.WriteValue(Type.ToString());
 
       writer.WritePropertyName("fillColor");
-      writer.WriteValue(FillColor);
+      writer.WriteValue(props.FillColor);
 
       writer.WritePropertyName("lineColor");
-      writer.WriteValue(LineColor);
+      writer.WriteValue(props.LineColor);
 
       writer.WritePropertyName("lineWidth");
-      writer.WriteValue(LineWidth);
+      writer.WriteValue(props.LineWidth);
 
       writer.WritePropertyName("visible");
       writer.WriteValue(true);
 
       writer.WritePropertyName("t");
       writer.WriteStartArray();
-      foreach (int t in GetTransformation(z)) {
+      foreach (int t in GetTransformation(props, z)) {
         writer.WriteValue(t);
       }
       writer.WriteEndArray();
@@ -238,20 +218,20 @@ namespace SimSharp.Visualization {
       return frame;
     }
 
-    private bool ShapesEqual() {
+    private bool ShapesEqual(AnimationProps props) {
       switch(Type) {
-        case Shape.rectangle: return Rectangle0.Equals(Rectangle1);
-        case Shape.ellipse: return Ellipse0.Equals(Ellipse1);
-        case Shape.polygon: return Polygon0.Equals(Polygon1);
+        case Shape.rectangle: return props.Rectangle0.Equals(props.Rectangle1);
+        case Shape.ellipse: return props.Ellipse0.Equals(props.Ellipse1);
+        case Shape.polygon: return props.Polygon0.Equals(props.Polygon1);
         default: return false;
       }
     }
 
-    private int[] GetTransformation(int z) {
+    private int[] GetTransformation(AnimationProps props, int z) {
       switch (Type) {
-        case Shape.rectangle: return z==0 ? Rectangle0.GetTransformation() : Rectangle1.GetTransformation();
-        case Shape.ellipse: return z == 0 ? Ellipse0.GetTransformation() : Ellipse1.GetTransformation();
-        case Shape.polygon: return z == 0 ? Polygon0.GetTransformation() : Polygon1.GetTransformation();
+        case Shape.rectangle: return z==0 ? props.Rectangle0.GetTransformation() : props.Rectangle1.GetTransformation();
+        case Shape.ellipse: return z == 0 ? props.Ellipse0.GetTransformation() : props.Ellipse1.GetTransformation();
+        case Shape.polygon: return z == 0 ? props.Polygon0.GetTransformation() : props.Polygon1.GetTransformation();
         default: return null;
       }
     }
--- src/SimSharp/Visualization/AnimationProps.cs
+++ src/SimSharp/Visualization/AnimationProps.cs
@@ -1,8 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SimSharp.Visualization.Shapes;
 
 namespace SimSharp.Visualization {
   class AnimationProps {
+    public Rectangle Rectangle0 { get; }
+    public Rectangle Rectangle1 { get; }
+
+    public Ellipse Ellipse0 { get; }
+    public Ellipse Ellipse1 { get; }
+
+    public Polygon Polygon0 { get; }
+    public Polygon Polygon1 { get; }
+
+    public string FillColor { get; }
+    public string LineColor { get; }
+    public int LineWidth { get; }
+    public DateTime Time0 { get; }
+    public DateTime Time1 { get; }
+    public bool Keep { get; }
+
+    public AnimationProps(Rectangle rectangle0, Rectangle rectangle1, DateTime time0, DateTime time1, string fillColor, string lineColor, int lineWidth, bool keep)
+      : this(fillColor, lineColor, lineWidth, time0, time1, keep) {
+      Rectangle0 = rectangle0;
+      Rectangle1 = rectangle1;
+    }
+
+    public AnimationProps(Ellipse ellipse0, Ellipse ellipse1, DateTime time0, DateTime time1, string fillColor, string lineColor, int lineWidth, bool keep)
+      : this(fillColor, lineColor, lineWidth, time0, time1, keep) {
+      Ellipse0 = ellipse0;
+      Ellipse1 = ellipse1;
+    }
+
+    public AnimationProps(Polygon polygon0, Polygon polygon1, DateTime time0, DateTime time1, string fillColor, string lineColor, int lineWidth, bool keep)
+      : this(fillColor, lineColor, lineWidth, time0, time1, keep) {
+      Polygon0 = polygon0;
+      Polygon1 = polygon1;
+    }
+
+    private AnimationProps(string fillColor, string lineColor, int lineWidth, DateTime time0, DateTime time1, bool keep) {
+      FillColor = fillColor;
+      LineColor = lineColor;
+      LineWidth = lineWidth;
+      Time0 = time0;
+      Time1 = time1;
+      Keep = keep;
+    }
   }
 }
